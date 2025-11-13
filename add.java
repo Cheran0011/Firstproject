8f3e80191f8285cc893ddb9ef705edd11a08e2ad
Cheran.G <146083120+Cheran0011@users.noreply.github.com>
2025-11-13 17:49:21 +0530

    change a

--- add.java
+++ add.java
@@ -2,10 +2,10 @@ public class add
 {
     public static void main(String[] args) 
     {
-        int a=100,b=20;
+        int a=22,b=20;
         int c=a+b;
         System.out.println(c);
-    }
+    
     
 }
 
